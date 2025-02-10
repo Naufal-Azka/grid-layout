00ed5eca7fbd9435cee4486d1e7607fce13a6996
Naufal Azka <askanaufal74@gmail.com>
2025-02-11 05:46:49 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
-const closeButton = document.getElementsByClassName("close-btn")[0]
-const modal = document.getElementById("modal")
+const modalForm = document.getElementById("modal-form")
 const addButton = document.getElementById("add-btn")
+const closeButtonForm = document.getElementById("close-btn-form")
 
-closeButton.onclick = function () {
-    modal.style.display = "none"
+// const modalCharacter = document.getElementById("modal-Character")
+// const characterCard = document.getElementById('character-container')
+// const closeButtonCharacter = document.getElementById('close-btn-character')
+
+// function openModalCharacter() {
+//     modalCharacter.style.display = "block"
+// }
+// function closeModalCharacter() {
+//     modalCharacter.style.display = "none"
+// }
+// characterCard.onclick = openModalCharacter
+// closeButtonCharacter.onclick = closeModalCharacter
+
+function openModal() {
+    modalForm.style.display = "block"
+    document.body.style.overflow = "hidden";
 }
-addButton.onclick = function () {
-    modal.style.display = "block"
+function closeModal() {
+    modalForm.style.display = "none"
+    document.body.style.overflow = "auto";
+    discardForm()
+}
+
+
+closeButtonForm.onclick = closeModal
+closeButtonForm.ontouchstart = closeModal
+
+addButton.onclick = openModal
+addButton.ontouchstart = openModal
+
+const imageInput = document.querySelector("input[id='operator-image']")
+const imagePreview = document.getElementById("image-preview")
+
+imageInput.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block';
+        }
+        reader.readAsDataURL(file);
+    } else {
+        imagePreview.src = '#';
+        imagePreview.style.display = 'none';
+    }
+});
+
+function discardForm() {
+    imageInput.value = ''
+    imagePreview.src = '#'
+    imagePreview.style.display = 'none'
+
+    document.getElementById('operator-form').reset()
 }
 
 let factionLists = document.getElementById('faction-lists')
@@ -20,7 +69,6 @@ fetch('src/factionOptions.json')
             factionLists.appendChild(option)
         })
     })
-
 
 let classLists = document.getElementById('class-lists')
 fetch('src/classOptions.json')
@@ -52,8 +100,7 @@ classLists.addEventListener("change", function() {
                 });
             }
         });
-});
-
+})
 
 let characterLists = JSON.parse(localStorage.getItem('characterLists')) || [];
 
@@ -65,6 +112,7 @@ function addCharacter(event) {
     event.preventDefault()
 
     const operatorName = document.getElementById('operator-name').value;
+    const operatorImage = document.getElementById('operator-image').value;
     const operatorFaction = document.getElementById('faction-lists').value;
     const operatorClass = document.getElementById('class-lists').value;
     const operatorBranch = document.getElementById('branch-lists').value;
@@ -73,6 +121,7 @@ function addCharacter(event) {
 
     let newCharacter = {
         name: operatorName,
+        image: operatorImage,
         faction: operatorFaction,
         class: operatorClass,
         branch: operatorBranch,
@@ -84,5 +133,16 @@ function addCharacter(event) {
     saveCharacter();
 
     document.getElementById('operator-form').reset()
-    modal.style.display = "none"
+    modalForm.style.display = "none"
 }
+
+modalForm.addEventListener('submit', addCharacter)
+
+function deleteCharacter(index) {
+    characterLists.splice(index, 1)
+    saveCharacter()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // deleteCharacter()
+})
